1ae9daa22cb6871d419b45d2cbe8bf13ace5975b
NyxInDark <meghdadi81@gmail.com>
2026-06-25 16:10:40 +0330

    Install react-icons and clsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Navbar from "@/components/layout/Navbar";
+import Hero from "@/components/home/Hero";
+import Footer from "@/components/layout/Footer";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Doctor Appointment</h1>
-    </main>
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
   );
 }
